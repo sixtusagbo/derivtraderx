edb73448ff3086e3a17dadede378c86eb99411a3
sixtusagbo <miracleagbosixtus@gmail.com>
2022-08-13 16:36:07 +0100

    Refactored homepage

--- resources/views/layouts/core.blade.php
+++ resources/views/layouts/core.blade.php
@@ -43,7 +43,165 @@
     {{-- @include('shared.loader') --}}
     @include('shared.header')
 
+    <!-- Hero -->
+    <div class="hero">
+        <!-- Node Particles -->
+        <div id="particles-js">
+        </div>
+
+        <div class="hero-content">
+            <h2 class="fw-bold text-light">Professional</h2>
+            <h1 class="fw-bold text-primary">Asset Management</h1>
+            <p class="fs-4 text-center">Up to 20% daily earnings, Instant withdrawal. Payments are in your account
+                regularly!
+            </p>
+            @if (Route::has('login'))
+                <div class="auth-buttons">
+                    @auth
+                        <a href="{{ url('/home') }}" class="btn btn-primary">Dashboard</a>
+                    @else
+                        <a href="{{ route('login') }}" class="btn btn-outline-primary">Log in</a>
+
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}" class="btn btn-primary">Register</a>
+                        @endif
+                    @endauth
+                </div>
+            @endif
+        </div>
+    </div>
+    <!-- Hero END -->
+
+    <!-- TradingView Widget BEGIN -->
+    <div class="tradingview-widget-container">
+        <div class="tradingview-widget-container__widget"></div>
+        <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-tickers.js" async>
+            {
+                "symbols": [{
+                    "description": "BTC/USD",
+                    "proName": "BITSTAMP:BTCUSD"
+                }, {
+                    "description": "ETH/USD",
+                    "proName": "KRAKEN:ETHUSD"
+                }, {
+                    "description": "LTC/USD",
+                    "proName": "KRAKEN:LTCUSD"
+                }, {
+                    "description": "BNB/USD",
+                    "proName": "BINANCE:BNBUSD"
+                }, {
+                    "description": "DOGE/USD",
+                    "proName": "BITTREX:DOGEUSD"
+                }, {
+                    "description": "TRX/USDT",
+                    "proName": "BINGX:TRXUSDT"
+                }],
+                "showSymbolLogo": true,
+                "colorTheme": "dark",
+                "isTransparent": true,
+                "displayMode": "adaptive",
+                "locale": "en"
+            }
+        </script>
+    </div>
+    <!-- TradingView Widget END -->
+
+    <!-- Google Translate -->
+    <div id="google_translate_element"></div>
+
     @yield('content')
+
+    <section class="pre-footer">
+        <div class="container">
+            <div class="accepted-methods d-flex justify-content-evenly py-4">
+                <img src="{{ asset('images/supported-systems/bitcoin.svg') }}" alt="">
+                <img src="{{ asset('images/supported-systems/ethereum.svg') }}" alt="">
+                <img src="{{ asset('images/supported-systems/litecoin.svg') }}" alt="">
+                <img src="{{ asset('images/supported-systems/bnb.png') }}" alt="" height="24"
+                    width="88">
+                <img src="{{ asset('images/supported-systems/doge.png') }}" alt="" height="24"
+                    width="88">
+                <img src="{{ asset('images/supported-systems/tron.svg') }}" alt="" height="24"
+                    width="88">
+                <img src="{{ asset('images/supported-systems/perfect_money.svg') }}" alt="">
+            </div>
+
+            <div class="more-info mt-3">
+                <div class="row text-left py-4">
+                    <div class="col-lg-3">
+                        <p class="fs-5 lead">Most profitable investment platform.</p>
+                    </div>
+                    <div class="col-lg-3">
+                        <h4 class="fs-2 fw-bold text-light mb-4">Get in Touch</h4>
+
+                        <ul class="navbar-nav d-flex justify-content-center">
+                            <li class="nav-item">
+                                <a href="tel:" class="link-secondary text-decoration-none"> <span
+                                        class="material-icons fs-5">call</span> Whatsapp </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="mailto:support@derivtraderx.com" class="link-secondary text-decoration-none">
+                                    <span class="material-icons fs-5">email</span> support@derivtraderx.com </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="mailto:support@derivtraderx.com" class="link-secondary text-decoration-none">
+                                    <span class="material-icons fs-5">location_on</span> 8564 Sumac Dr
+                                    Baldwinsville, New York(NY), 13027 </a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="col-lg-3">
+                        <h4 class="fs-2 fw-bold text-light mb-4">Quick Links</h4>
+
+                        <ul class="navbar-nav d-flex justify-content-center">
+                            <li class="nav-item">
+                                <a href="{{ url('/') }}" class="link-secondary text-decoration-none"> <span
+                                        class="material-icons fs-5">home</span> Home </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="{{ url('about') }}" class="link-secondary text-decoration-none"> <span
+                                        class="material-icons fs-5">info</span> About Us </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="{{ url('faq') }}" class="link-secondary text-decoration-none"> <span
+                                        class="material-icons fs-5">help</span> FAQ </a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="col-lg-3">
+                        <h4 class="fs-2 fw-bold text-light mb-4">Help Desk</h4>
+
+                        <ul class="navbar-nav d-flex justify-content-center">
+                            <li class="nav-item">
+                                <a href="{{ url('contact') }}" class="link-secondary text-decoration-none"> <span
+                                        class="material-icons fs-5">perm_contact_calendar</span> Contact </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="{{ route('login') }}" class="link-secondary text-decoration-none"> <span
+                                        class="material-icons fs-5">login</span> Login </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="{{ route('register') }}" class="link-secondary text-decoration-none"> <span
+                                        class="material-icons fs-5">how_to_reg</span> Sign Up </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <footer>
+        <div class="container">
+            <div class="d-flex justify-content-between py-4 bg-dark">
+                <a href="{{ url('terms') }}" class="link-primary text-decoration-none">Terms & conditions</a>
+
+                <p>&copy; 2022 All Rights Reserved | <a href="{{ url('/') }}"
+                        class="link-primary text-decoration-none">DerivTraderx</a>
+                </p>
+            </div>
+        </div>
+    </footer>
 
     <!-- Scripts -->
     <script src="{{ asset('plugin/jquery.js') }}"></script>
@@ -51,6 +209,7 @@
     <script src="{{ asset('plugin/owl.carousel.min.js') }}"></script>
     <script src="{{ asset('plugin/aos.js') }}"></script>
     <script src="{{ asset('js/app.js?v=') . time() }}"></script>
+    @include('shared.other_scripts')
     @yield('scripts')
 </body>
 
---
+++ resources/views/shared/other_scripts.blade.php
@@ -0,0 +1,25 @@
+<script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit">
+</script>
+
+<script type="text/javascript">
+    // Google Translate Scripts
+    function googleTranslateElementInit() {
+        new google.translate.TranslateElement({
+            pageLanguage: 'en'
+        }, 'google_translate_element');
+    }
+
+    // Start of Tawk.to Script
+    var Tawk_API = Tawk_API || {},
+        Tawk_LoadStart = new Date();
+    (function() {
+        var s1 = document.createElement("script"),
+            s0 = document.getElementsByTagName("script")[0];
+        s1.async = true;
+        s1.src = 'https://embed.tawk.to/62018946b9e4e21181bdeb32/1frb00fok';
+        s1.charset = 'UTF-8';
+        s1.setAttribute('crossorigin', '*');
+        s0.parentNode.insertBefore(s1, s0);
+    })();
+    // End of Tawk.to Script
+</script>
