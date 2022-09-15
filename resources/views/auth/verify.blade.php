b2a8bf8d63f27ad06b33f05f4c4dc702a5a7dec4
sixtusagbo <miracleagbosixtus@gmail.com>
2022-09-15 17:57:06 +0100

    [Update] Redesign email verification page

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -1,28 +1,18 @@
-@extends('layouts.app')
+@extends('layouts.auth')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Verify Your Email Address') }}</div>
-
-                <div class="card-body">
-                    @if (session('resent'))
-                        <div class="alert alert-success" role="alert">
-                            {{ __('A fresh verification link has been sent to your email address.') }}
-                        </div>
-                    @endif
-
-                    {{ __('Before proceeding, please check your email for a verification link.') }}
-                    {{ __('If you did not receive the email') }},
-                    <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
-                        @csrf
-                        <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>.
-                    </form>
-                </div>
-            </div>
+    <h1 class="sign-up__title fw-bold">Verify Your Email Address</h1>
+    <p class="sign-up__subtitle">Before proceeding, please check your email for a verification link. <br>If you did not
+        receive the email</p>
+    @if (session('resent'))
+        <div class="alert alert-success" role="alert">
+            {{ __('A fresh verification link has been sent to your email address.') }}
         </div>
-    </div>
-</div>
+    @endif
+
+    <form class="sign-up-form form" method="POST" action="{{ route('verification.resend') }}">
+        @csrf
+
+        <button class="form-btn primary-default-btn transparent-btn">Click here to request another</button>
+    </form>
 @endsection
