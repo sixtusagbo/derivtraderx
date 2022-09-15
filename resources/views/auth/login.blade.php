6c4d6bfb4161dec3bc582935f8bff11f27f36483
sixtusagbo <miracleagbosixtus@gmail.com>
2022-09-15 13:05:46 +0100

    [Update] Redesign login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,46 @@
-@extends('layouts.app')
+@extends('layouts.auth')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+    <h1 class="sign-up__title fw-bold">Welcome back!</h1>
+    <p class="sign-up__subtitle">Sign in to your account and continue the adventure of making money from investment🚀
+    </p>
+    <form class="sign-up-form form" method="POST" action="{{ route('login') }}">
+        @csrf
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+        <label class="form-label-wrapper">
+            <p class="form-label">Email Address</p>
+            <input class="form-input @error('email') is-invalid @enderror" type="email" placeholder="Enter your email"
+                value="{{ old('email') }}" autocomplete="email" required>
 
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
+            @error('email')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
+        </label>
+        <label class="form-label-wrapper">
+            <p class="form-label">Password</p>
+            <input class="form-input @error('password') is-invalid @enderror" type="password"
+                placeholder="Enter your password" autocomplete="current-password" required>
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+            @error('password')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
+        </label>
 
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
+        @if (Route::has('password.request'))
+            <a class="link-info forget-link text-primary"
+                href="{{ route('password.request') }}">{{ __('Forgot Your Password?') }}</a>
+        @endif
+        <label class="form-checkbox-wrapper">
+            <input class="form-checkbox" type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }} required>
+            <span class="form-checkbox-label">Remember me next time</span>
+        </label>
 
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+        <button class="form-btn primary-default-btn transparent-btn">Sign in</button>
+        <p class="mt-4 text-center text-light">New to derivtraderx? <a href="{{ route('register') }}"
+                class="link-primary text-decoration-none">Create an account</a></p>
+    </form>
 @endsection
